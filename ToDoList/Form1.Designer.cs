d40d3429b9cd4fd8803d6b2f15888395bb029e6a
Caliari <jrcalliari@me.com>
2016-11-21 22:03:49 +0000

    Upload theToDoList prototype

--- ToDoList/Form1.Designer.cs
+++ ToDoList/Form1.Designer.cs
@@ -28,12 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(497, 69);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(515, 41);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(136, 69);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add Task";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(249, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "To Do List";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnRemove);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 116);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(639, 58);
+            this.panel1.TabIndex = 3;
+            this.panel1.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 7;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(546, 18);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 26);
+            this.btnRemove.TabIndex = 6;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(76, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(114, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 4;
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(101, 9);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 23);
+            this.btnHelp.TabIndex = 4;
+            this.btnHelp.Text = "Help ?";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(663, 295);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "To do List Project";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ToDoList/Form1.cs
+++ ToDoList/Form1.cs
@@ -12,9 +12,220 @@ namespace ToDoList
 {
     public partial class Form1 : Form
     {
+
+
         public Form1()
         {
             InitializeComponent();
+            
         }
+
+        String task;
+        int taskNumber = 0;
+        int location = 1;
+        int indexId = 1;
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+          //  AddNewPanel();
+        }
+
+        //private void AddNewPanel()
+        //{
+        //    taskNumber++;
+        //    label3.Text = taskNumber.ToString();
+
+        //    task = richTextBox1.Text;
+        //    label2.Text = task;
+
+        //    panel1.Visible = true;
+        //    richTextBox1.Text = null;
+        //    checkBox1.Visible = true;
+
+        //}
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "")
+            {
+                MessageBox.Show("Please, Write a task! ");
+            }
+            else
+            {
+                AddNewTaskContents();
+                richTextBox1.Text = null;
+            }
+    
+        }
+
+        
+
+        private void AddNewTaskContents()
+        {
+            //
+            //Create a Label counter inside the panel
+            //
+            Label newLabel = new Label();
+            newLabel.Name = "labelCounter" + indexId;
+            newLabel.AutoSize = true;
+            newLabel.Location = new System.Drawing.Point(17, 14);
+            newLabel.Size = new System.Drawing.Size(0, 13);
+
+            taskNumber++;
+            newLabel.Text = taskNumber.ToString();
+
+            //
+            //Create a checkBox inside the panel
+            //
+            CheckBox newCheckBox = new CheckBox();
+            newCheckBox.Name = "CheckBox" + indexId;
+            newCheckBox.AutoSize = true;
+            newCheckBox.Location = new System.Drawing.Point(76, 14);
+            newCheckBox.Size = new System.Drawing.Size(15, 14);
+            newCheckBox.TabIndex = 5;
+            newCheckBox.UseVisualStyleBackColor = true;
+            newCheckBox.Visible = true;
+            newCheckBox.CheckedChanged += new EventHandler(newcheckBox_CheckedChanged);
+
+            //
+            //Create a Label task inside the panel
+            //
+            Label newLabelTask = new Label();
+            newLabelTask.Name = "LabelTask" + indexId;
+            newLabelTask.AutoSize = true;
+            newLabelTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            //new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            newLabelTask.Location = new System.Drawing.Point(114, 12);
+            newLabelTask.Size = new System.Drawing.Size(0, 16);
+            newLabelTask.Visible = true;
+            newLabelTask.TabIndex = 4;
+
+            task = richTextBox1.Text;
+            newLabelTask.Text = task;
+
+
+            //
+            //Create a RichText task inside the panel
+            //
+            //RichTextBox newRichText = new RichTextBox();
+            //newRichText.Name = "RichText" + indexId;
+            //newRichText.Location = new System.Drawing.Point(117, 3);
+            //newRichText.Size = new System.Drawing.Size(380, 52);
+            //newRichText.TabIndex = 8;
+            //newRichText.Visible = true;
+
+            //task = richTextBox1.Text;
+            //newRichText.Text = task;
+
+            //
+            //Create a Remove button inside the panel
+            //
+            Button btnRemove = new Button();
+            btnRemove.Name = "Remove" + indexId;
+            btnRemove.Text = "Remove"; //+ indexId.ToString();
+            btnRemove.Size = new System.Drawing.Size(75, 26);
+            btnRemove.Location = new System.Drawing.Point(546, 18);
+            btnRemove.UseVisualStyleBackColor = true;
+            btnRemove.Visible = true ;
+            btnRemove.Click += new EventHandler(btnRemove_Click);
+
+            //
+            //Create a Panel for all conponents 
+            //
+            Panel newPanel = new Panel();
+            newPanel.Name = "Panel" + indexId;
+            newPanel.Text = "newPanel" + indexId.ToString();
+            newPanel.Location = new System.Drawing.Point(12, 58 + (60 * location));
+            newPanel.Size = new System.Drawing.Size(639, 58);
+            newPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+
+            //
+            //Adding all the components in the form when the 'ADD Task' button gets pressed
+            //
+            newPanel.Controls.Add(newLabel);
+            newPanel.Controls.Add(newCheckBox);
+            newPanel.Controls.Add(newLabelTask);
+            //newPanel.Controls.Add(newRichText);
+            newPanel.Controls.Add(btnRemove);
+
+            Controls.Add(newPanel);
+
+            indexId++;
+            location++;
+        }
+
+        //
+        //The Remove button fecture is working as expected
+        //
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("working 0");
+            
+           string senderIndex = ((Button)sender).Name.Remove(0,6); //'.Name.Remove(0, 6)' removes the button's name from 0 to 6th character
+
+           Controls.RemoveByKey("Panel" + senderIndex);
+
+            //MessageBox.Show("" + senderIndex);
+
+            indexId--;
+            location--;
+            taskNumber--;
+
+            //Make sure the location is '1' can not be '0'
+            //taskNumber must start from '1' e can't be negative number  
+            //indexId must start from '1' e can't be negative number
+            if (location < 1 || taskNumber < 1 || indexId < 1)
+            {
+                taskNumber = 0;
+                location = 1;
+                indexId = 1;
+            }
+
+            MessageBox.Show("index" + indexId);
+        }
+
+        //
+        //The checkBox fecture is still under developmenting, not working as expected
+        //
+        private void newcheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox newCheckBox = new CheckBox();
+            newCheckBox.Name = ((CheckBox)sender).Name;
+            // MessageBox.Show("working 1");
+
+            //  int senderIndex = Convert.ToInt32(((CheckBox)sender).Name.Remove(0, 8));
+
+            string senderIndex = ((CheckBox)sender).Name;
+
+           
+            MessageBox.Show(senderIndex);
+
+            newCheckBox.Enabled = true;
+
+        }
+
+        //END
+
+        //
+        //The bellow code is just a testing for the feacture that is being implemented
+        //
+
+
+        //private void btnRemove_Click(object sender, EventArgs e)
+        //{
+        //    this.Visible = false;
+        //    checkBox1.Checked = false;
+        //    checkBox1.Enabled = true;
+        //    label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+        //}
+
+        //private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Strikeout, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        //    //newCheckBox.Enabled = false;
+        //}
+
     }
 }
